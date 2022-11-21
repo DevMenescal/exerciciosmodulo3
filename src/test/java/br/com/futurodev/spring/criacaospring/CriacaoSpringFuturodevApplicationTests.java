d8e1be14af96a42c436ab79c01f6e84e1fa950d8
Gustavo Menescal <menescal@gmail.com>
2022-11-21 11:15:02 -0300

    Criação do Spring Initializr

---
+++ src/test/java/br/com/futurodev/spring/criacaospring/CriacaoSpringFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.spring.criacaospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoSpringFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
